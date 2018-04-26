2038a6ab9a149e1d35d756d58edb52d1c5039eaa
Dupuy, Maxime (CHICO) <maxime.dupuy@hyatt.com>
2018-04-25 19:40:54 -0500

    End of lesson 3

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { link } from 'react-router';
+
+
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="photo-grid">
+            grid
+            </div>
+        )
+    }
+})
+
+export default PhotoGrid;
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { link } from 'react-router';
+
+
+const Single = React.createClass({
+    render() {
+        return (
+            <div className="single-photo">
+            single
+            </div>
+        )
+    }
+})
+
+export default Single;
